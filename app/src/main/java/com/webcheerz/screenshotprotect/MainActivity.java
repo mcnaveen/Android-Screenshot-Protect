ba27b8286692e7097de0fed54ffe3ccf5e29b56b
MC Naveen <funmemcn@gmail.com>
2021-01-28 19:02:15 +0530

    :tada: Initial

---
+++ app/src/main/java/com/webcheerz/screenshotprotect/MainActivity.java
@@ -0,0 +1,17 @@
+package com.webcheerz.screenshotprotect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+    }
+}
